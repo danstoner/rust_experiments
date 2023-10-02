f1a6faa01ff4093439ca78cdb0e4084683eab601
Dan Stoner <dstoner@iris.washington.edu>
2023-10-02 09:14:19 -0400

    add variables tutorial

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,57 @@
+use std::{io, process::exit};
+
+
 fn main() {
-    println!("Hello, world!");
+
+    // let mut x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is: {x}");
+
+
+
+
+
+    println!("============");
+    println!("");
+
+
+
+    println!("Please input first number:");
+
+    let mut line: String = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read input");
+
+    let first: i8 = match line.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("ERROR: Must type a number!");
+            exit(1);
+        }
+    };
+
+    line.clear();
+    println!("Please input second number:");
+
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read input");
+
+    let second: i8 = match line.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("ERROR: Must type a number!");
+            exit(1);
+        }
+    };
+
+    println!("{first}, {second}");
+    println!("Addition result: {}", add(first,second));
+}
+
+
+fn add (a: i8, b: i8) -> i8 {
+    return a + b;
 }
